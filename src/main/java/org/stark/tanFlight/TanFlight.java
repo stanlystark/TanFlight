96544719077f8bbdf0bbcf1b8f10609426009655
Stark <adm@justenjoy.ru>
2025-04-21 14:45:11 +0300

    /tanfly command

--- src/main/java/org/stark/tanFlight/TanFlight.java
+++ src/main/java/org/stark/tanFlight/TanFlight.java
@@ -1,17 +1,46 @@
 package org.stark.tanFlight;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.stark.tanFlight.commands.TflyCommand;
+import org.stark.tanFlight.listeners.FlightRegionListener;
+import org.stark.tanFlight.utils.CommandRegistrar;
+import java.util.logging.Logger;
 
 public final class TanFlight extends JavaPlugin {
 
+    public static final Logger LOGGER = Logger.getLogger(TanFlight.class.getName());
+
+    private static TanFlight instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        if (isTanPluginMissing()) {
+            LOGGER.severe("Cannot find Towns and Nations...");
+            setEnabled(false);
+            return;
+        }
 
+        instance = this;
+        saveDefaultConfig();
+
+        LOGGER.info("Plugin enabled.");
+
+        CommandRegistrar.registerCommand(this, "tanfly", new TflyCommand(), "Flight", null, new TflyCommand());
+        getServer().getPluginManager().registerEvents(new FlightRegionListener(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        LOGGER.info("Plugin disabled.");
+    }
+
+    public static TanFlight getInstance() {
+        return instance;
+    }
+
+    private boolean isTanPluginMissing() {
+        Plugin tanPlugin = getServer().getPluginManager().getPlugin("TownsAndNations");
+        return tanPlugin == null || !tanPlugin.isEnabled();
     }
 }
