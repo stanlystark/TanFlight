f8a2b4214e71d9ee95f04129f023b59166cf23ac
Stark <adm@justenjoy.ru>
2025-04-21 09:13:21 +0300

    init

---
+++ src/main/java/org/stark/tanFlight/TanFlight.java
@@ -0,0 +1,17 @@
+package org.stark.tanFlight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TanFlight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
